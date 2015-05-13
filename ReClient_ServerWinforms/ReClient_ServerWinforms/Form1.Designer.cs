416c2bba295939bfef38235e1c3c1b742071fbb7
JAVED IQAL <engrjavediqbal837@yahoo.com>
2015-05-13 11:16:24 +0500

    first commit

---
+++ ReClient_ServerWinforms/ReClient_ServerWinforms/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ReClient_ServerWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.send = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "From Server";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "From Client:Type here and send";
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(309, 134);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 5;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 233);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(251, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(309, 230);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(230, 301);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 336);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ReClient_ServerWinforms/ReClient_ServerWinforms/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+namespace ReClient_ServerWinforms
+{
+    public partial class Form1 : Form
+    {
+        private static string shortFileName = "";
+        private static string fileName = "";
+        TcpClient clientSocket = new TcpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Thread()
+        {
+
+
+            IPAddress ipaddr = IPAddress.Parse("127.0.0.1");
+
+            TcpListener serverSocket = new TcpListener(ipaddr, 8004);
+
+            int requestCount = 0;
+
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+
+            Console.WriteLine(" >> Server Started");
+
+            clientSocket = serverSocket.AcceptTcpClient();
+
+            Console.WriteLine(" >> Accept connection from client");
+
+            requestCount = 0;
+
+            #pragma warning disable
+
+            while ((true))
+            {
+
+                try
+                {
+
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[clientSocket.ReceiveBufferSize];
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    MessageBox.Show(dataFromClient);
+                    string serverResponse = " Last Message from client " + dataFromClient;
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+
+                }
+
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.ToString());
+
+                }
+
+            }
+
+            //  clientSocket.Close();
+
+            // serverSocket.Stop();
+
+            Console.WriteLine(" >> exit");
+            #pragma warning restore
+
+            Console.ReadLine();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            ThreadStart threaddelegate = new ThreadStart(Thread);
+             Thread newthread = new Thread(threaddelegate);
+             newthread.Start(); 
+
+            msg("Client Started");
+
+            clientSocket.Connect("127.0.0.1", 8004);
+
+            label1.Text = "Client Socket Program - Server Connected ...";
+
+        }
+
+        public void msg(string mesg)
+        {
+
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+
+            NetworkStream serverStream = clientSocket.GetStream();
+
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+
+            serverStream.Write(outStream, 0, outStream.Length);
+
+            serverStream.Flush();
+
+
+
+            byte[] inStream = new byte[clientSocket.ReceiveBufferSize];
+
+            serverStream.Read(inStream, 0, inStream.Length);
+
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+
+            msg(returndata);
+
+            textBox2.Text = "";
+
+            textBox2.Focus();
+            
+
+
+        }
+        private void btnBrowse_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "File Sharing Client";
+            dlg.ShowDialog();
+            textBox3.Text = dlg.FileName;
+            fileName = dlg.FileName;
+            shortFileName = dlg.SafeFileName;
+
+        }
+
+        private void btnSend_Click_1(object sender, EventArgs e)
+        {
+            string ipAddress = "127.0.0.1";
+            int port = 8004;
+            string fileName = textBox3.Text;
+            
+                Task.Factory.StartNew(() => SendFile(ipAddress, port, fileName, shortFileName));
+                MessageBox.Show("File Sent");
+          
+            
+
+        }
+        public void SendFile(string remoteHostIP, int remoteHostPort, string longFileName, string shortFileName)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(remoteHostIP))
+                {
+                    byte[] fileNameByte = Encoding.ASCII.GetBytes(shortFileName);
+                    byte[] fileData = File.ReadAllBytes(longFileName);
+                    byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
+                    byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);                    
+                    fileNameLen.CopyTo(clientData, 0);
+                    fileNameByte.CopyTo(clientData, 4);
+                    fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+                    TcpClient clientSocket = new TcpClient(remoteHostIP, remoteHostPort);
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Write(clientData, 0, clientData.GetLength(0));
+                    networkStream.Close();
+                }
+            }
+            catch (Exception exx)
+            {
+
+                MessageBox.Show(exx.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
